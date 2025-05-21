48aa70fd5f4f87d3b750e40440bc98833b739fd6
Steaven <mirandsteavenjohann@gmail.com>
2025-05-21 18:34:08 +0800

    remove gatekeepr backend in url

--- accounts/email.py
+++ accounts/email.py
@@ -1,20 +1,19 @@
 from djoser.email import ActivationEmail, PasswordResetEmail
 from django.core.mail import EmailMultiAlternatives
 
-class CustomActivationEmail(ActivationEmail):
-    def get_context_data(self):
-        context = super().get_context_data()
-        context['url'] = f"https://gatekeepr1.netlify.app/activate/{context['uid']}/{context['token']}"
-        return context
 
+class CustomActivationEmail(ActivationEmail):
     def send(self, to, *args, **kwargs):
-        context = self.get_context_data()
+        uid = self.context['uid']
+        token = self.context['token']
+        activation_url = f"https://gatekeepr1.netlify.app/activate/{uid}/{token}"
+
         subject = "Activate your Gatekeepr account"
         body = (
-            f"Hi {context['user'].get_full_name()},\n\n"
+            f"Hi {self.context['user'].get_full_name()},\n\n"
             f"Thanks for signing up!\n"
             f"Please activate your account by clicking the link below:\n\n"
-            f"{context['url']}\n\n"
+            f"{activation_url}\n\n"
             f"— The Gatekeepr Team"
         )
         msg = EmailMultiAlternatives(subject, body, self.from_email, [to])
@@ -22,19 +21,17 @@ class CustomActivationEmail(ActivationEmail):
 
 
 class CustomPasswordResetEmail(PasswordResetEmail):
-    def get_context_data(self):
-        context = super().get_context_data()
-        context['url'] = f"https://gatekeepr1.netlify.app/password/reset/confirm/{context['uid']}/{context['token']}"
-        return context
-
     def send(self, to, *args, **kwargs):
-        context = self.get_context_data()
+        uid = self.context['uid']
+        token = self.context['token']
+        reset_url = f"https://gatekeepr1.netlify.app/password/reset/confirm/{uid}/{token}"
+
         subject = "Reset your Gatekeepr password"
         body = (
-            f"Hi {context['user'].get_full_name()},\n\n"
+            f"Hi {self.context['user'].get_full_name()},\n\n"
             f"You requested a password reset. Click the link below to set a new password:\n\n"
-            f"{context['url']}\n\n"
-            f"If you didn’t request this, you can safely ignore this email.\n\n"
+            f"{reset_url}\n\n"
+            f"If you didn’t request this, you can ignore this email.\n\n"
             f"— The Gatekeepr Team"
         )
         msg = EmailMultiAlternatives(subject, body, self.from_email, [to])
