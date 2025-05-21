804a8d0e8c7a668ff43ff676622c974c5e162628
Steaven <mirandsteavenjohann@gmail.com>
2025-05-21 19:01:55 +0800

    again edit sa email

--- accounts/email.py
+++ accounts/email.py
@@ -4,9 +4,11 @@ from django.core.mail import EmailMultiAlternatives
 class CustomActivationEmail(ActivationEmail):
     def get_context_data(self):
         context = super().get_context_data()
-        # Explicitly set the frontend URL without the backend domain
+        # Set the full frontend URL
         context['url'] = f"https://gatekeepr1.netlify.app/activate/{context['uid']}/{context['token']}"
-        context['domain'] = None  # Prevent Djoser from prepending the backend domain
+        # Explicitly disable domain and protocol to prevent prepending
+        context['domain'] = ''
+        context['protocol'] = ''
         context['site_name'] = 'Gatekeepr'
         return context
 
@@ -14,7 +16,7 @@ class CustomActivationEmail(ActivationEmail):
         context = self.get_context_data()
         subject = "Activate your Gatekeepr account"
         body = (
-            f"Hi {context['user'].get_full_name()},\n\n"
+            f"Hi {context['user'].get_full_name() or context['user'].username},\n\n"
             f"Thanks for signing up!\n"
             f"Please activate your account by clicking the link below:\n\n"
             f"{context['url']}\n\n"
@@ -26,9 +28,11 @@ class CustomActivationEmail(ActivationEmail):
 class CustomPasswordResetEmail(PasswordResetEmail):
     def get_context_data(self):
         context = super().get_context_data()
-        # Explicitly set the frontend URL without the backend domain
+        # Set the full frontend URL
         context['url'] = f"https://gatekeepr1.netlify.app/password/reset/confirm/{context['uid']}/{context['token']}"
-        context['domain'] = None  # Prevent Djoser from prepending the backend domain
+        # Explicitly disable domain and protocol to prevent prepending
+        context['domain'] = ''
+        context['protocol'] = ''
         context['site_name'] = 'Gatekeepr'
         return context
 
@@ -36,7 +40,7 @@ class CustomPasswordResetEmail(PasswordResetEmail):
         context = self.get_context_data()
         subject = "Reset your Gatekeepr password"
         body = (
-            f"Hi {context['user'].get_full_name()},\n\n"
+            f"Hi {context['user'].get_full_name() or context['user'].username},\n\n"
             f"You requested a password reset. Click the link below to set a new password:\n\n"
             f"{context['url']}\n\n"
             f"If you didn’t request this, you can safely ignore this email.\n\n"
