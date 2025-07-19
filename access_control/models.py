c47654a405e31e013cabf211197d9b4260069d8d
Steaven <mirandsteavenjohann@gmail.com>
2025-07-19 15:17:33 +0800

    location parking

--- access_control/models.py
+++ access_control/models.py
@@ -33,14 +33,24 @@ class ParkingSlot(models.Model):
         ('OPEN', 'Open'),
     ]
 
+    LOCATION_CHOICES = [
+        ('BUILDING_A', 'Building A'),
+        ('BUILDING_B', 'Building B'),
+        ('BUILDING_C', 'Building C'),
+        ('BUILDING_D', 'Building D'),
+        ('BUILDING_E', 'Building E'),
+        ('BUILDING_F', 'Building F'),
+        ('ADMIN', 'Admin'),
+    ]
+
     slot_number = models.CharField(max_length=10, unique=True, null=True, blank=True)
     status = models.CharField(max_length=10, choices=[('AVAILABLE', 'Available'), ('OCCUPIED', 'Occupied')])
     type = models.CharField(max_length=10, choices=SLOT_TYPES, default='OPEN')
-    resident = models.ForeignKey(Resident, on_delete=models.SET_NULL, null=True, blank=True)
+    resident = models.ForeignKey('Resident', on_delete=models.SET_NULL, null=True, blank=True)
+    location = models.CharField(max_length=20, choices=LOCATION_CHOICES, null=True, blank=True)
 
     def __str__(self):
         return f"Slot No: {self.slot_number} - {self.status} - {self.get_type_display()}"
-
 
 class AccessLog(models.Model):
     ENTRY = 'ENTRY'
