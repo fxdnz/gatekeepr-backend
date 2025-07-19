5a1a333b84f918ffe8c629302278bd2156960614
Steaven <mirandsteavenjohann@gmail.com>
2025-07-19 11:55:14 +0800

    cors allowed all origins true

--- gatekeepr/settings.py
+++ gatekeepr/settings.py
@@ -50,15 +50,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:57617",
-    "https://gatekeepr1.netlify.app",
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:8000",
+#     "http://localhost:57617",
+#     "https://gatekeepr1.netlify.app",
     
-]
+# ]
 
-CORS_ALLOW_ALL_ORIGIN = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'gatekeepr.urls'
 
